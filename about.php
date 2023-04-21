31ce5ef4175c1c257fbf8bda2f88d7f84a99b7f3
utkirkurbanov <utkirkurbanov2700@gmail.com>
2023-04-21 22:05:21 +0500

    Working with blocks

---
+++ about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <title>Project001</title>
+</head>
+<body>
+    <?php require "blocks/header.php" // header'imizni require orqali about.php'ga chaqirib oldik?>
+        
+    <?php require "blocks/footer.php" // header'imizni require orqali footer.php'ga chaqirib oldik?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -9,17 +9,7 @@
     <title>Project001</title>
 </head>
 <body>
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-      <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
-        <span class="fs-4">RiseSoft</span>
-      </a>
-
-      <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-        <a class="me-3 py-2 text-dark text-decoration-none" href="#">Header</a>
-        <a class="me-3 py-2 text-dark text-decoration-none" href="#">Contact</a>
-      </nav>
-      <a class="btn btn-outline-primary" href="#">Enter</a>
-    </div>
+    <?php require "blocks/header.php" // header'imizni require orqali index.php'ga chaqirib oldik?> 
     <div class="container mt-5">
         <h3 class="mb-5">Our articles</h3>
 
@@ -68,42 +58,7 @@
             <?php endfor; // sikl tugadi?> 
         </div>        
     </div>
-    <footer class="container pt-4 my-md-5 pt-md-5 border-top">
-    <div class="row">
-      <div class="col-12 col-md">
-        <span class="fs-4">RiseSoft</span><br>
-        <small class="d-block mb-3 text-muted">© 2017–2022</small>
-      </div>
-      <div class="col-6 col-md">
-        <h5>Features</h5>
-        <ul class="list-unstyled text-small">
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Cool stuff</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Random feature</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Team feature</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Stuff for developers</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Another one</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Last time</a></li>
-        </ul>
-      </div>
-      <div class="col-6 col-md">
-        <h5>Resources</h5>
-        <ul class="list-unstyled text-small">
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Resource</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Resource name</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Another resource</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Final resource</a></li>
-        </ul>
-      </div>
-      <div class="col-6 col-md">
-        <h5>About</h5>
-        <ul class="list-unstyled text-small">
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Team</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Locations</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Privacy</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Terms</a></li>
-        </ul>
-      </div>
-    </div>
-   </footer>
+    
+    <?php require "blocks/footer.php" // header'imizni require orqali footer.php'ga chaqirib oldik?>
 </body>
 </html>
